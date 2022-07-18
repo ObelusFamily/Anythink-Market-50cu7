69cfc7d67a9618723d2cf22ed8aa5698b075622f
omer bental <ombental@gmail.com>
2022-07-18 19:31:00 +0300

    add onerror handling to image of item -> use placeholder.png

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -29,6 +29,7 @@ const ItemPreview = (props) => {
     }
   };
 
+  console.log(item.image)
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,6 +38,10 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; // prevents looping
+          currentTarget.src="/placeholder.png";
+        }}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
