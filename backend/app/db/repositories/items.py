3f7573bb5534bc0eafca5ca9be8c32acc7f501c0
omer bental <ombental@gmail.com>
2022-07-23 10:58:18 +0300

    fix filter to use LIKE + add "%"

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -139,11 +139,11 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         # fmt: on
 
         if title:
-            query_params.append(title)
+            query_params.append(title + '%')
             query_params_count += 1
 
             # fmt: off
-            query = query.where(items.title == Parameter(query_params_count))
+            query = query.where(items.title.like(Parameter(query_params_count)))
             # fmt: on
 
         if tag:
